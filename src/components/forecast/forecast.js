0874f68a5e7472594cb768d4e32e154499ea0e12
kemvick <ayebaifiemivictor.com>
2024-06-26 14:14:18 +0100

    Initial commit

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -3,17 +3,36 @@ import '../../index.css'
 import './forecast.css'
 import weathercond1 from '../../assets/Small Moon cloud mid rain.png'
 import weathercond2 from '../../assets/Moon cloud mid rain.png'
-import weathercond3 from '../../assets/Sun cloud mid rain.png'
+import { FaThermometerEmpty } from 'react-icons/fa'
+import { FiWind } from 'react-icons/fi'
+import { BiSolidDropletHalf } from 'react-icons/bi'
+import { GiSun, GiSunrise } from 'react-icons/gi'
+
 const Forecast = () => {
-  const weatherConditions = [
+  const weatherStatDetails = [
     {
-      condition1: weathercond1,
+      id: 1,
+      Icon: FaThermometerEmpty,
+      title: 'Real feel',
+      value: '15 °C',
     },
     {
-      condition2: weathercond2,
+      id: 2,
+      Icon: BiSolidDropletHalf,
+      title: 'Humidity',
+      value: '93%',
     },
     {
-      condition3: weathercond3,
+      id: 3,
+      Icon: FiWind,
+      title: 'Wind',
+      value: '13 km/h',
+    },
+    {
+      id: 4,
+      Icon: GiSunrise,
+      title: 'Sunrise',
+      value: '9:19 Am',
     },
   ]
   return (
@@ -24,12 +43,25 @@ const Forecast = () => {
         </div>
         <div className='temp-and-details'>
           <p className='city-name'>London, GB</p>
-          <img src={weatherConditions.condition1} alt='rainy' />
+          <img src={weathercond1} alt='rainy' className='weather-img' />
           <h1 className='temp'>19°</h1>
           <p className='weather-desc'>mostly clear</p>
           <p className='high-low'>
             <p>H-24°</p> L-18°
           </p>
+          <div className='weather-stats-container'>
+            {weatherStatDetails.map(({ id, Icon, title, value }) => (
+              <div key={id} className='weather-stat'>
+                <Icon className='cond-img' />
+                <p>{title}</p>
+                <p>{value}</p>
+              </div>
+            ))}
+          </div>
+          <div className='weather-map'>
+            weather map
+            <div className='map'>hbjbjbejb</div>
+          </div>
         </div>
       </section>
     </>
